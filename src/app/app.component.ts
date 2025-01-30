33aea0587fbad2e46eabf09b24d71054c1eaa987
Nils_de_Turckheim <nils.turckheim@gmail.com>
2025-01-30 14:39:22 +1000

    🧱 Update to fetch and display the classes and families depending

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,27 +5,47 @@ import { Observable } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 import { ClassList } from '../domain/class';
 import { FamilyList } from '../domain/family';
+import { ClassService } from '../services/service.class';
+import { fromSubscribable } from 'rxjs/internal/observable/fromSubscribable';
+import { FamilyService } from '../services/service.family';
 
 @Component({
   selector: 'app-root',
   imports: [AsyncPipe],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-
-
 export class AppComponent implements OnInit {
+  title = 'wild-life-integration';
 
   public kingdoms$!: Observable<KingdomList>;
   public classes$!: Observable<ClassList>;
   public families$!: Observable<FamilyList>;
 
+  public kingdomCommonName: string = "";
+  public classCommonName: string = "";
+  public familyCommonName: string = "";
 
   private kingdomService = inject(KingdomService);
+  private classService = inject(ClassService);
+  private familyService = inject(FamilyService);
 
   ngOnInit() {
     this.kingdoms$ = this.kingdomService.getKingdomList();
   }
 
-  title = 'wild-life-integration';
+  onKingdomClick(kingdomCommonName: string) {
+    this.kingdomCommonName = kingdomCommonName
+    this.classes$ = this.classService.getClassList(kingdomCommonName);
+    this.families$ = new Observable<FamilyList>();
+  }
+
+  onClassClick(classCommonName: string) {
+    this.classCommonName = classCommonName;
+    this.families$ = this.familyService.getFamilyList(this.kingdomCommonName, classCommonName);
+  }
+  
+  onFamilyClick(familyCommonName: string) {
+    console.log(familyCommonName);
+  }
 }
