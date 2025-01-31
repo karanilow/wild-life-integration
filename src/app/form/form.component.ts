30a8dff0b581352a6d2c79027e40f98ccd98a14c
Nils_de_Turckheim <nils.turckheim@gmail.com>
2025-01-31 21:56:40 +1000

    🙋 Create a new component for the form

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { KingdomList } from '../../domain/kingdom';
+import { KingdomService } from '../../services/service.kingdom';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
+import { ClassList } from '../../domain/class';
+import { FamilyList } from '../../domain/family';
+import { ClassService } from '../../services/service.class';
+import { fromSubscribable } from 'rxjs/internal/observable/fromSubscribable';
+import { FamilyService } from '../../services/service.family';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  imports: [AsyncPipe],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.css',
+})
+export class FormComponent implements OnInit {
+  public kingdoms$!: Observable<KingdomList>;
+  public classes$!: Observable<ClassList>;
+  public families$!: Observable<FamilyList>;
+
+  public kingdomCommonName: string = '';
+  public classCommonName: string = '';
+  public familyCommonName: string = '';
+
+  private kingdomService = inject(KingdomService);
+  private classService = inject(ClassService);
+  private familyService = inject(FamilyService);
+
+  ngOnInit() {
+    this.kingdoms$ = this.kingdomService.getKingdomList();
+  }
+
+  onKingdomClick(kingdomCommonName: string) {
+    this.kingdomCommonName = kingdomCommonName;
+    this.classes$ = this.classService.getClassList(kingdomCommonName);
+    this.families$ = new Observable<FamilyList>();
+  }
+
+  onClassClick(classCommonName: string) {
+    this.classCommonName = classCommonName;
+    this.families$ = this.familyService.getFamilyList(
+      this.kingdomCommonName,
+      classCommonName
+    );
+  }
+
+  onFamilyClick(familyCommonName: string) {
+    console.log(familyCommonName);
+  }
+}
