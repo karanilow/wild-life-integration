d6e29ec98078e61a19be1c7d75e590ee6d22f8ec
Nils_de_Turckheim <nils.turckheim@gmail.com>
2025-02-11 14:16:53 +1000

    ➖ Delete unused pages

--- src/app/pages/ui-components/ui-components.routes.ts
+++ src/app/pages/ui-components/ui-components.routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
 
 // ui
-import { AppBadgeComponent } from './badge/badge.component';
-import { AppChipsComponent } from './chips/chips.component';
-import { AppListsComponent } from './lists/lists.component';
-import { AppMenuComponent } from './menu/menu.component';
-import { AppTooltipsComponent } from './tooltips/tooltips.component';
 import { AppFormsComponent } from './forms/forms.component';
-import { AppTablesComponent } from './tables/tables.component';
 import { AppSpecieComponent } from './specie/specie.component';
 
 export const UiComponentsRoutes: Routes = [
